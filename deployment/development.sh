b32336d771b8516ab9167ab802bee8ea15d44b1a
Payam Moghaddam <payam.moghaddam@accenture.com>
2013-12-14 17:01:02 -0800

    Updated deployment script

--- deployment/development.sh
+++ deployment/development.sh
@@ -8,14 +8,16 @@ grunt
 echo ">> Update submodules"
 git submodule init
 git submodule update
+
+echo ">> Ensure submodule is on 'master'"
 cd server
 git checkout master
 
 echo ">> Commit Heroku Entry"
 rm -rf dist
 cp -R ../dist dist
-
-echo ">> Pushing to Heroku"
 git add -A
 git commit -m "Another build"
+
+echo ">> Pushing to Heroku"
 git push origin master --force
