63a892ecce87f503001d0ceb2197b247bb9e0b16
Payam Moghaddam <payam.moghaddam@accenture.com>
2013-12-16 01:48:41 -0800

    Add basic Socket.IO integration

--- README.md
+++ README.md
@@ -18,3 +18,9 @@ For distribution, `grunt` task creates a `dist` folder with files processed for 
 The production server itself is a basic `express` Node.JS application that simply serves the content of the `dist` application. This is captured as a submodule in the `server` application.
 
 To achieve this, the Continuous Integration server simply executes `sh deployment/development.sh` which brings the latest files into the `server` folder, commits the changes, and then pushes them out to the submodule's master branch (Heroku).
+
+# Tasks
+* [X] Add Socket.IO
+* Provide basic functionality
+* Understand how to keep socket.io as separate file? (prevent large JS files)
+* Utilize CryptoJS (AES-256 bit)
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -17,12 +17,43 @@ require.config({
     paths: {
         jquery: '../bower_components/jquery/jquery',
         backbone: '../bower_components/backbone/backbone',
-        underscore: '../bower_components/underscore/underscore'
+        underscore: '../bower_components/underscore/underscore',
+        socketio: '../bower_components/socket.io-client/dist/socket.io'
     }
 });
 
 require([
-    'backbone'
-], function (Backbone) {
+    'jquery',
+    'backbone',
+    'socketio'
+], function ($, Backbone, io) {
     Backbone.history.start();
+
+    // Local or not
+    var url;
+    if (document.location.hostname == "localhost") {
+        url = 'http://localhost:5000';
+    } else {
+        url = 'http://blpm-api.herokuapp.com';
+    }
+
+    // Dummy View Area
+    var $view = $('#list');
+
+    // Basic Socket.IO connection
+    var socket = io.connect(url);
+    window.socket = socket;
+    socket.on('connected', function (data) {
+        console.log(data);
+    });
+    socket.on('tasks:list', function(tasks) {
+        for (var i = 0; i < tasks.length; i++) {
+            $view.append('<li>' + tasks[i].title + '</li>');
+        }
+    });
+    socket.on('tasks:create', function(task) {
+        $view.prepend('<li>' + task.title + '</li>');
+    });
+
+    socket.emit('tasks:list');
 });
